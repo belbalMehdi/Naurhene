e16f1774dec02ea7e124294a1647fe20104461d4
BELBAL <mehdi.belbal@atos.net>
2017-05-10 17:42:38 +0200

    hello

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var tabCols = [
             [1,3,1,1,0,0]
           ];
 
-var tabRaws = [
+var tabRows = [
             [1,1,1,2,0,0],
             [1,1,1,0,0,0],
             [1,1,1,1,0,0],
             [1,1,1,1,0,0],
-            [1,1,1,1,1,1],
+            [1,1,0,1,1,1],
             [1,1,1,1,0,0]
           ];
 
@@ -25,9 +25,9 @@ var draw = function(){
     game += "<tr>";
     for(var j=0;j<tabCols[i].length;j++)
     {
-      if(tabCols[i][j]>1||tabRaws[i][j]>1) color="yellow";
+      if(tabCols[i][j]>1||tabRows[i][j]>1) color="yellow";
       else color = "black";
-      if(tabCols[i][j]>0) game+='<td colspan="'+tabCols[i][j]+'" rowspan="'+tabRaws[i][j]+'" style="background-color:'+color+'"></td>';
+      if(tabCols[i][j]>0) game+='<td colspan="'+tabCols[i][j]+'" rowspan="'+tabRows[i][j]+'" style="background-color:'+color+'"></td>';
     }
     game += "</tr>";
   }
@@ -44,7 +44,7 @@ document.addEventListener('click',function(e){
       if(e.target==tableau.querySelector("tbody").childNodes[i].childNodes[j])
       {
           memory.i = i;
-          memory.j = j;
+          memory.j = trouverPosition(i,j);
       }
     }
   }
@@ -54,7 +54,7 @@ document.addEventListener('keyup',function(e){
   if(e.key=="ArrowRight"){
     if(tabCols[memory.i][memory.j]>1)
     {
-      if(tabCols[memory.i][memory.j+1]==1&&tabRaws[memory.i][memory.j+1]==1){
+      if(tabCols[memory.i][memory.j+1]==1&&tabRows[memory.i][memory.j+1]==1){
         ancienneValeur = tabCols[memory.i][memory.j];
         tabCols[memory.i][memory.j]=1;
         tabCols[memory.i][memory.j+1] = ancienneValeur;
@@ -64,14 +64,29 @@ document.addEventListener('keyup',function(e){
   if(e.key=="ArrowLeft"){
     if(tabCols[memory.i][memory.j]>1)
     {
-      if(tabCols[memory.i][memory.j-1]==1&&tabRaws[memory.i][memory.j-1]==1){
+      if(tabCols[memory.i][memory.j-1]==1&&tabRows[memory.i][memory.j-1]==1){
         ancienneValeur = tabCols[memory.i][memory.j];
         tabCols[memory.i][memory.j]=1;
         tabCols[memory.i][memory.j-1] = ancienneValeur;
       }
     }
   }
+  if(e.key=="ArrowDown"){
+    var taille = tabRows[memory.i][memory.j];
+    if(taille>1)
+    {
+      if(tabCols[memory.i+taille][memory.j]==1&&tabRows[memory.i+taille][memory.j]==1){
+        ancienneValeur = taille;
+        tabRows[memory.i][memory.j]=1;
+        tabRows[memory.i+1][memory.j] = ancienneValeur;
+        tabCols[memory.i+1][memory.j] = 1;
+        tabRows[memory.i+taille][memory.j] = 0;
+        tabCols[memory.i+taille][memory.j] = 0;
+      }
+    }
+  }
   reorder();
+  showTables();
   draw();
 })
 
@@ -82,9 +97,42 @@ var reorder = function(){
       if(tabCols[i][j]==0){
         tabCols[i].splice(j,1);
         tabCols[i].push(0);
+        tabRows[i].splice(j,1);
+        tabRows[i].push(0);
       }
     }
   }
 }
 
-reorder();
+//reorder();
+
+var showTables = function(){
+  resultat = "";
+  resultat2 = "";
+  for(var i=0;i<tabCols.length;i++){
+    for(var j=0;j<tabCols[i].length;j++)
+    {
+      resultat += tabCols[i][j] + ' ,';
+      resultat2 += tabRows[i][j] + ' ,';
+    }
+    resultat += "\n";
+    resultat2 += "\n";
+  }
+  console.info("####################################");
+  console.log(resultat);
+  console.log(resultat2);
+}
+
+var trouverPosition = function(i,pos){
+  var k=-1;
+  var z=0;
+  for(var j=0;j<tabCols[i].length;j++)
+  {
+    if(tabCols[i][j]>0)k++;
+    if(k==pos){
+      z=j;
+      break;
+    }
+  }
+  return z;
+}
