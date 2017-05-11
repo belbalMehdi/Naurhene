acdc177d1af08d30dda259bda302295a3180868e
BELBAL <mehdi.belbal@atos.net>
2017-05-11 16:37:30 +0200

    85%==>DONE

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 var tabCols = [
-            [2,1,2,1,0,0],
-            [1,2,2,0,0,0],
-            [1,2,2,1,0,0],
-            [1,1,3,1,0,0],
-            [1,1,0,2,1,1],
-            [1,3,1,1,0,0]
+            [3,0,0,1,1,1],
+            [2,0,2,0,0,1],
+            [2,0,1,1,0,1],
+            [2,0,1,1,1,1],
+            [1,1,1,1,2,0],
+            [1,0,0,3,0,0]
           ];
 
 var tabRows = [
-            [1,1,1,2,0,0],
-            [1,1,1,0,0,0],
-            [1,1,1,1,0,0],
-            [1,1,1,1,0,0],
-            [1,1,0,1,1,1],
-            [1,1,1,1,0,0]
+            [1,0,0,1,3,1],
+            [1,0,1,0,0,1],
+            [1,0,1,1,0,1],
+            [1,1,1,1,1,0],
+            [1,2,2,1,1,0],
+            [1,0,0,1,0,0]
           ];
 
 var tableau = document.querySelector("#page")
+
+var reorder = function(){
+  for(var i=0;i<tabCols.length;i++){
+    for(var j=0;j<tabCols[i].length;j++)
+    {
+      if(tabCols[i][j]==0){
+        tabCols[i].splice(j,1);
+        tabCols[i].push(0);
+        tabRows[i].splice(j,1);
+        tabRows[i].push(0);
+      }
+    }
+  }
+}
+
+reorder();
 
 var draw = function(){
   var game = "";
@@ -35,6 +51,7 @@ var draw = function(){
 }
 draw();
 
+
 var memory = {};
 document.addEventListener('click',function(e){
   memory.target = e.target;
@@ -44,7 +61,7 @@ document.addEventListener('click',function(e){
       if(e.target==tableau.querySelector("tbody").childNodes[i].childNodes[j])
       {
           memory.i = i;
-          memory.j = trouverPosition(i,j);
+          memory.j = trouverPositionCols(i,j);
       }
     }
   }
@@ -75,13 +92,33 @@ document.addEventListener('keyup',function(e){
     var taille = tabRows[memory.i][memory.j];
     if(taille>1)
     {
-      if(tabCols[memory.i+taille][memory.j]==1&&tabRows[memory.i+taille][memory.j]==1){
+      if(tabCols[memory.i+taille][trouverPositionRows(memory,taille)]==1&&tabRows[memory.i+taille][trouverPositionRows(memory,taille)]==1){
         ancienneValeur = taille;
         tabRows[memory.i][memory.j]=1;
-        tabRows[memory.i+1][memory.j] = ancienneValeur;
-        tabCols[memory.i+1][memory.j] = 1;
-        tabRows[memory.i+taille][memory.j] = 0;
-        tabCols[memory.i+taille][memory.j] = 0;
+        tabCols[memory.i][memory.j]=1;
+        tabRows[memory.i+1][trouverPositionRows(memory,1)] = ancienneValeur;
+        tabCols[memory.i+1][trouverPositionRows(memory,1)] = 1;
+        tabRows[memory.i+taille][trouverPositionRows(memory,taille)] = 0;
+        tabCols[memory.i+taille][trouverPositionRows(memory,taille)] = 0;
+      }
+    }
+  }
+  if(e.key=="ArrowUp"){
+    var taille = tabRows[memory.i][memory.j];
+    if(taille>1)
+    {
+      console.log("test taille passed");
+      if(tabCols[memory.i-1][trouverPositionRows(memory,-1)]==1&&tabRows[memory.i-1][trouverPositionRows(memory,-1)]==1){
+        console.log("test previous passed");
+        ancienneValeur = taille;
+        console.log((memory.i+taille-1)+' / '+memory.j);
+        tabRows[memory.i][memory.j]=0;
+        tabCols[memory.i][memory.j]=0;
+        console.log("-1 :::: "+trouverPositionRows(memory,-1));
+        tabRows[memory.i-1][trouverPositionRows(memory,-1)] = taille;
+        tabCols[memory.i-1][trouverPositionRows(memory,-1)] = 1;
+        tabRows[memory.i+taille-1][trouverPositionRows(memory,(taille-1))]=1;
+        tabCols[memory.i+taille-1][trouverPositionRows(memory,(taille-1))]=1;
       }
     }
   }
@@ -90,21 +127,6 @@ document.addEventListener('keyup',function(e){
   draw();
 })
 
-var reorder = function(){
-  for(var i=0;i<tabCols.length;i++){
-    for(var j=0;j<tabCols[i].length;j++)
-    {
-      if(tabCols[i][j]==0){
-        tabCols[i].splice(j,1);
-        tabCols[i].push(0);
-        tabRows[i].splice(j,1);
-        tabRows[i].push(0);
-      }
-    }
-  }
-}
-
-//reorder();
 
 var showTables = function(){
   resultat = "";
@@ -123,7 +145,7 @@ var showTables = function(){
   console.log(resultat2);
 }
 
-var trouverPosition = function(i,pos){
+var trouverPositionCols = function(i,pos){
   var k=-1;
   var z=0;
   for(var j=0;j<tabCols[i].length;j++)
@@ -131,6 +153,28 @@ var trouverPosition = function(i,pos){
     if(tabCols[i][j]>0)k++;
     if(k==pos){
       z=j;
+      break;
+    }
+  }
+  return z;
+}
+
+var trouverPositionRows = function(memo,offset){
+  posx = memo.i;
+  posy = memo.j;
+  console.log(posx+' / '+posy);
+  var somme = 0;
+  var somme2 = 0;
+  var z=0;
+  for(var j=0;j<posy;j++)
+  {
+    somme += tabCols[posx][j];
+  }
+  for(var j=0;j<tabCols[posx+offset].length;j++)
+  {
+    somme2 += tabCols[posx+offset][j];
+    if(somme==somme2){
+      z = j+1;
       break;
     }
   }
